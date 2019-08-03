07c676743b387ddffd43619be0cafa7dee0fce00
guanghao0419@outlook.com <1936381013@qq.com>
2019-08-02 22:23:29 -0700

    report view

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,6 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
-import pickle
-from app.sentiment import *
-
-filename = 'app/data/supervised.sav'
-cls_task2 = pickle.load(open(filename, 'rb'))
-
-with open("app/data/sentiment_task2.pickle", "rb") as f:
-    sentiment_task2 = pickle.load(f)
-
 from app import routes
 
 
